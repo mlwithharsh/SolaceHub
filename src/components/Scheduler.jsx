84f65d268eb6244d85acf681d980f5d2b5703242
Harsh Sharma <haresh2pandit@gmail.com>
2026-02-06 17:21:00 +0530

    re fix of time slot

--- src/components/Scheduler.jsx
+++ src/components/Scheduler.jsx
@@ -133,17 +133,17 @@ const Scheduler = () => {
                                                     onChange={(e) => setFormData({ ...formData, timeSlot: e.target.value })}
                                                 >
                                                     <option value="10:00 AM">10:00 AM</option>
-                                                    <option value="11:00 AM">10:00 AM</option>
-                                                    <option value="12:00 AM">10:00 AM</option>
-                                                    <option value="1:00 PM">10:00 AM</option>
+                                                    <option value="11:00 AM">11:00 AM</option>
+                                                    <option value="12:00 AM">12:00 AM</option>
+                                                    <option value="1:00 PM">1:00 PM</option>
                                                     <option value="2:00 PM">2:00 PM</option>
                                                     <option value="3:00 PM">3:00 PM</option>
-                                                    <option value="4:00 PM">10:00 AM</option>
-                                                    <option value="5:00 PM">10:00 AM</option>
-                                                    <option value="6:00 PM">10:00 AM</option>
-                                                    <option value="7:00 PM">10:00 AM</option>
-                                                    <option value="8:00 PM">10:00 AM</option>
-                                                    <option value="9:00 PM">10:00 AM</option>
+                                                    <option value="4:00 PM">4:00 PM</option>
+                                                    <option value="5:00 PM">5:00 PM</option>
+                                                    <option value="6:00 PM">6:00 PM</option>
+                                                    <option value="7:00 PM">7:00 PM</option>
+                                                    <option value="8:00 PM">8:00 PM</option>
+                                                    <option value="9:00 PM">9:00 PM</option>
                                                 </select>
                                                 <div className="absolute right-4 top-1/2 -translate-y-1/2 pointer-events-none text-slate-400">
                                                     ▼
