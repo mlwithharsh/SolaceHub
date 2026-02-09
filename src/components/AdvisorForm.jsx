f7a467426f611bfe37e22322aa660678863cfa32
Harsh Sharma <haresh2pandit@gmail.com>
2026-02-09 19:40:18 +0530

    Update AdvisorForm.jsx

--- src/components/AdvisorForm.jsx
+++ src/components/AdvisorForm.jsx
@@ -10,7 +10,8 @@ const AdvisorForm = () => {
         contact: ''
     });
     const [submitted, setSubmitted] = useState(false);
-
+    const [loading, setLoading] = useState(false)
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
 
