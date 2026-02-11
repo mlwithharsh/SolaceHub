713adc6be1c00ccbdd2b5d5b2913dfff12be2a77
Harsh Sharma <haresh2pandit@gmail.com>
2026-02-11 23:28:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/origa-logo.svg';
+import logo from '../assets/origa-logo.png';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
