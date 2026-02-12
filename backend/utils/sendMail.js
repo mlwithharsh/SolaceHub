1977ad92394eb0e8b393a2f03b94b3935c42fbde
Harsh Sharma <haresh2pandit@gmail.com>
2026-02-12 21:34:18 +0530

    Update sendMail.js

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendNotification = async (subject, html) => {
     try {
         await resend.emails.send({
-            from: "SolaceHub <onboarding@resend.dev>",
+            from: "SolaceHub <origa.net04@gmail.com>",
             to: process.env.ADMIN_EMAIL,
             subject,
             html,
